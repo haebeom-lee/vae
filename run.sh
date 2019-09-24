9f1bd844a9593326e0e9a83f29ae30105395cc8b
Hae Beom Lee <43218718+haebeom-lee@users.noreply.github.com>
2019-09-24 15:00:23 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,10 +10,10 @@ python run.py \
     --zdim 20 \
     --mode test
 
-python run.py \
-    --savedir "./results/run" \
-    --gpu_num -1 \
-    --zdim 20 \
-    --mode visualize \
-    --scatter \
-    --reconstruct
+#python run.py \
+#    --savedir "./results/run" \
+#    --gpu_num -1 \
+#    --zdim 20 \
+#    --mode visualize \
+#    --scatter \
+#    --reconstruct
